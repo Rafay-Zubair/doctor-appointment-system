42d10e78e9c9738cd3c15982d694b2a9c8d56e3b
Rafay-Zubair <zubairgillrafay@gmail.com>
2023-12-12 00:42:12 +0500

    This commit has connected doctor appointment microservice to mongodb

--- appointments/app.py
+++ appointments/app.py
@@ -1,27 +1,45 @@
 from flask import Flask, jsonify, request
+from pymongo import MongoClient
+import os
+
 app = Flask(__name__)
 
-appointments = [
-  { 'id': "1",'doctor': "1", 'date': "21 Nov 2023", 'rating':"Good"  },
-  { 'id': "2",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-  { 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
-  { 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-  { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
-]
+# Connection with MongoDB
+db_client = MongoClient(os.environ.get('HOST_URL'), int(os.environ.get('DB_PORT')))
+# Database
+db = db_client.doc_appointment_system
+# Collection
+appointment_collection = db.appointment
+
+if appointment_collection.count_documents({}) == 0:
+	# When appointments collection is empty
+	appointments = [
+		{ 'id': "1",'doctor': "1", 'date': "21 Nov 2023", 'rating':"Good"  },
+		{ 'id': "2",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
+		{ 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+		{ 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
+		{ 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+	]
+	for app in appointments:
+		appointment_collection.insert_one(app)
 
 @app.route('/hello')
 def hello():
-  greeting = "Hello world!"
-  return greeting
+	greeting = "Hello world!"
+	return greeting
 
 @app.route('/appointments', methods=["GET"])
 def getAppointments():
-  return jsonify(appointments)
+	data = list(appointment_collection.find())
+	for d in data:
+		d.pop("_id")
+	return jsonify(data)
 
 @app.route('/appointment/<id>', methods=["GET"])
 def getAppointment(id):
-  id = int(id) - 1
-  return jsonify(appointments[id])
+	data = appointment_collection.find_one({'id':str(id)})
+	data.pop("_id")
+	return jsonify(data)
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=7070)
+	app.run(host="0.0.0.0",port=7070)
--- doctors/app.py
+++ doctors/app.py
@@ -1,23 +1,18 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
-# doctors = [
-#   { 'id': "1",'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality':"DevOps"  },
-#   { 'id': "2",'firstName': "Good", 'lastName': "Doctor",'speciality':"Test"  }
-# ]
-
 # Connection with MongoDB
-db_client = MongoClient(process.env.HOST_URL, process.env.DB_PORT)
+db_client = MongoClient(os.environ.get('HOST_URL'), int(os.environ.get('DB_PORT')))
 # Database
 db = db_client.doc_appointment_system
 # Collection
 doc_collection = db.doctor
 
-data = doc_collection.find()
-if data.len() == 0:
-	# If doctors collection is empty
+if doc_collection.count_documents({}) == 0:
+	# When doctors collection is empty
 	doctors = [
 		{ 'id': "1",'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality':"DevOps"  },
 		{ 'id': "2",'firstName': "Good", 'lastName': "Doctor",'speciality':"Test"  }
@@ -27,20 +22,21 @@ if data.len() == 0:
 
 @app.route('/hello')
 def hello():
-  greeting = "Hello world!"
-  return greeting
+	greeting = "Hello world!"
+	return greeting
 
 @app.route('/doctors', methods=["GET"])
 def getDoctors():
-	data = doc_collection.find()
-  return jsonify(data)
+	data = list(doc_collection.find())
+	for d in data:
+		d.pop("_id")
+	return jsonify(data)
 
 @app.route('/doctor/<id>', methods=["GET"])
 def getDoctor(id):
 	data = doc_collection.find_one({'id':str(id)})
+	data.pop("_id")
 	return jsonify(data)
-  # id = int(id) - 1
-  # return jsonify(doctors[id])
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=9090)
+	app.run(host="0.0.0.0",port=9090)
